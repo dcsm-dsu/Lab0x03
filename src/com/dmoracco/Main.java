867f3b4f3e2303b18c75d4e19f4c3838e7df3692
dcsm-dsu <david.moracco@trojans.dsu.edu>
2020-10-04 18:56:24 -0500

    Imported previous sort methods from lab0x02

--- src/com/dmoracco/Main.java
+++ src/com/dmoracco/Main.java
@@ -22,6 +22,20 @@ public class Main {
         long maxTime = 10000000;
         int n = 1000000;
 
+        int[] sortlist = generateList(10);
+        for (int num :
+                sortlist) {
+            System.out.print(num + ", ");
+        }
+        System.out.println();
+        sortlist = MergeSort(sortlist);
+        for (int num :
+                sortlist) {
+            System.out.print(num + ", ");
+        }
+
+
+/*
         System.out.printf("\n\n%20s%20s%20s%20s\n", "N", "Time (m/s)", "Ratio", "Exp. Ratio");
 
         for (int i = 1; i < n; i=i*2){
@@ -40,8 +54,8 @@ public class Main {
 
             System.out.printf("%20s%20s%20s%20s\n", i, currentTime, ratio, "8");
             lastTime = currentTime;
-
         }
+*/
     }
 
     public static void validate() {
@@ -113,5 +127,77 @@ public class Main {
         }
 
         return sumList;
+    }
+
+    public static ArrayList ThreeSumFasterBinary(int[] list, int n){
+        ArrayList sumList = new ArrayList();
+
+        // Sort list first
+
+
+
+        return sumList;
+    }
+
+    static int[] MergeSort(int[] list){
+
+        int size = list.length;
+
+        // One or less is sorted by definition
+        if (size <= 1) return list;
+
+        // Create new arrays
+        int split = (int) Math.ceil(size/2.0); // get proper size for odd N, 2.0 cause int arithmetic apparently...
+        int[] left = new int[split];
+        int[] right = new int[size/2];
+
+        // Split list
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < size; i++){
+            if (i < split) left[l++] = list[i];
+            else right[r++] = list[i];
+        }
+
+        // Recursive calls
+        left = MergeSort(left);
+        right = MergeSort(right);
+
+        // Merge sorted lists
+        return mergeArrays(left, right);
+
+        // Psuedocode from https://en.wikipedia.org/wiki/Merge_sort
+    }
+
+    static int[] mergeArrays(int[] left, int[] right){
+
+        int l = 0, r = 0, m = 0;
+        int lsize = left.length;
+        int rsize = right.length;
+        int[] mergedList = new int[lsize + rsize];
+
+        // iterate through list until either list is complete
+        while (l < lsize && r < rsize){
+            // determine order to fill mergedList
+            if (left[l] <= right[r]) mergedList[m++] = left[l++];
+            else mergedList[m++] = right[r++];
+        }
+
+        // Handle remainder for odd N lists
+        while (l < lsize) mergedList[m++] = left[l++];
+        while (r < rsize) mergedList[m++] = right[r++];
+
+        return mergedList;
+    }
+
+    static boolean needsSwapped(byte[] previousList, byte[] currentList) {
+        int i = -1;
+        // return immediately if out of order, otherwise increment through array
+        do {
+            i++;
+            if (previousList[i] > currentList[i]) return true;
+        } while (i < previousList.length && previousList[i] == currentList[i]);
+
+        return false;
     }
 }
